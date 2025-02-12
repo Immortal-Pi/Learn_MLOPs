c01b89ced64a77f112017d9e6dd24c755c032a97
amruth pai <26.amruth@gmail.com>
2025-02-12 03:35:12 -0600

    'dockers'

--- 6.Dockers/app.py
+++ 6.Dockers/app.py
@@ -1,9 +1,14 @@
 import flask
-
+import pickle
 
 from flask import Flask
 import os 
 app=Flask(__name__)
+
+# load the classifier model
+# pickle_in=open('classifier.pkl','rb')
+# classifier=pickle.load(pickle_in)
+
 
 @app.route('/',methods=['GET'])
 def home():
